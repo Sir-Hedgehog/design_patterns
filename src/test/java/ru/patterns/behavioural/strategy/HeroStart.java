5a9a687e482cfd765c56a6888f0f951fba7d9fba
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2020-10-15 22:37:46 +0300

    Strategy

--- src/test/java/ru/patterns/behavioural/strategy/HeroStart.java
+++ src/test/java/ru/patterns/behavioural/strategy/HeroStart.java
@@ -6,23 +6,37 @@ import static org.junit.Assert.assertThat;
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version 1.0
+ * @version 2.0
  * @since 15.10.2020
  */
 
 public class HeroStart {
     @Test
-    public void checkDifferentActivitiesOfHero() {
+    public void checkProtectionOfHero() {
         Hero achilles = new Hero();
         achilles.setActivity(new Protection());
         achilles.takeAction();
         assertThat(achilles.takeAction(), is("Protects the weak people!"));
+    }
+
+    @Test
+    public void checkSalvationOfHero() {
+        Hero achilles = new Hero();
         achilles.setActivity(new Salvation());
         achilles.takeAction();
         assertThat(achilles.takeAction(), is("Save the desperate people!"));
+    }
+
+    @Test
+    public void checkFightOfHero() {
+        Hero achilles = new Hero();
         achilles.setActivity(new Fight());
         achilles.takeAction();
         assertThat(achilles.takeAction(), is("Fight against evil!"));
+    }
+    @Test
+    public void checkSleepingOfHero() {
+        Hero achilles = new Hero();
         achilles.setActivity(new Sleeping());
         achilles.takeAction();
         assertThat(achilles.takeAction(), is("Sometimes sleeps..."));
